984745c4d447d39b7b2a93dcd00d1704c1aab2cd
emloxe <qwrtyp@live.cn>
2019-04-16 00:58:59 +0800

    添加反向代理

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const compression = require('compression');
+const express = require("express");
+const compression = require("compression");
+const timeout = require("connect-timeout");
+const proxy = require("http-proxy-middleware");
 const app = express();
 
+// 设置超时 返回超时响应
+const TIME_OUT = 30 * 1e3;
+app.use(timeout(TIME_OUT));
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
+
 app.use(compression()); // 压缩
-app.use(function(req, res, next) { // 允许跨域
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+app.use(function(req, res, next) {
+  // 允许跨域
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 app.use(express.static(__dirname)); // 当前目录作为资源目录
-app.use(express.static('assets')); // 指定目录作为资源目录
-app.use('/js', express.static('dist'));  // 指定目录添加路由前缀
+app.use(express.static("assets")); // 指定目录作为资源目录
+app.use("/js", express.static("dist")); // 指定目录添加路由前缀
 
-app.listen('8080', function(req, res){
-	console.log('connect to http://127.0.0.1:8080')
-})
+// 反向代理
+const proxyPath = "http://192.168.0.48:9005"; // 目标后端服务地址(公司同事电脑地址)
+const proxyOption = { target: proxyPath, changeOrigoin: true };
+app.use(proxy("/api/test", proxyOption)); // 这里要注意"/api/test" 是匹配的路由,它会将匹配的路由进行转发，没匹配到的就不会转发。('/discern'完全可以写成'/'就是说所有路由都可以访问)
+
+app.listen("8080", function(req, res) {
+  console.log("connect to http://127.0.0.1:8080");
+});
